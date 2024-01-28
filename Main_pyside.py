5f32fab7111e3a42552c7584dca98d9545add529
Aens <Aenswindstorm@gmail.com>
2024-01-28 19:59:13 +0100

    botones funcionales

--- Main_pyside.py
+++ Main_pyside.py
@@ -6,8 +6,7 @@ from pathlib import Path
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import QSize, QPoint
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
-
+from PySide6.QtWidgets import QApplication, QInputDialog, QLineEdit
 
 __VERSION__ = "v0.3"
 __AUTHOR__ = "Aens"
@@ -18,10 +17,15 @@ class Notepad:
 
     def __init__(self):
         """Load fields from a text file"""
+        self.gui = None
         self.notes = {}
         self.folderpath = Path.cwd().joinpath("notes")
         self.deleted_folderpath = Path.cwd().joinpath("old_notes")
         self.reload_notes()
+
+    def add_gui_pointer(self, gui):
+        """Add a gui pointer just to call stuff from this class"""
+        self.gui = gui
 
     def reload_notes(self):
         """Clean the previous list. Crawl to get all the notes. Load them into the class"""
@@ -34,15 +38,33 @@ class Notepad:
                 content = f.read()
                 self.notes[title] = content
 
-    def delete_note(self, name):
+    def add_note(self, new_name):
+        """Adds a new note"""
+        new_note_path = self.folderpath.joinpath(f"{new_name}.txt")
+        if new_note_path.exists():
+            self.gui.show_popup("Error, esa nota ya existe")
+            return
+        else:
+            try:
+                with open(new_note_path, 'w') as file:
+                    file.write("")  # Creating an empty note for now
+                print(f"Note '{new_name}' added successfully.")
+            except Exception as e:
+                print(f"Error creating note '{new_name}': {e}")
+
+    def delete_note(self, name: str) -> None:
         """It doesn't delete notes, it just moves them to a different folder"""
         source_filepath = self.folderpath.joinpath(f"{name}.txt")
         destination_filepath = self.deleted_folderpath.joinpath(f"{name}.txt")
         source_filepath.rename(destination_filepath)
         print(f"File moved from {source_filepath} to {destination_filepath}")
 
-    def edit_note(self, key):
-        print(f"Edit button clicked for note: {key}")
+    def save_note(self, filename: str, value: str) -> None:
+        """Saves a note with this new values"""
+        new_note_path = self.folderpath.joinpath(f"{filename}.txt")
+        with open(new_note_path, 'w') as file:
+            file.write(value)  # Overwriting the content of that note
+        print(f"Nota '{filename}' guardada con exito.")
 
 
 class GUI(QtWidgets.QMainWindow):
@@ -54,6 +76,7 @@ class GUI(QtWidgets.QMainWindow):
         self.app = app
         self.config_file = 'program_settings.ini'
         self.notepad = notepad  # Pointer to our notepad
+        self.notepad.add_gui_pointer(self)  # Reverse pointer to our gui
         self.DEFAULT_WINDOW_SIZE = QSize(800, 600)
         self.DEFAULT_WINDOW_POSITION = QPoint(200, 200)
         self.DEFAULT_WINDOW_TRANSPARENCY = 0.8
@@ -89,16 +112,18 @@ class GUI(QtWidgets.QMainWindow):
                 self.save_window_config()  # Store the window size to the config file
         return super().eventFilter(watched, event)
 
-
     def create_layout(self):
+        """Creates the layout and sets it"""
         central_widget = QtWidgets.QWidget(self)
         self.setCentralWidget(central_widget)
 
         main_layout = QtWidgets.QGridLayout(central_widget)
 
-        add_note_button = QtWidgets.QPushButton('Add Note')
+        add_note_button = QtWidgets.QPushButton('Añadir otra nota')
         add_note_button.clicked.connect(self.add_note)
-        deleted_notes_label = QtWidgets.QLabel("Deleted notes are just moved to the Old Notes folder")  # TODO fix this
+        deleted_notes_label = QtWidgets.QLabel(
+            "Las notas borradas no se 'borran', se almacenan en la carpeta old_notes "
+            "por si acaso borras una sin querer.")
         deleted_notes_label.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
 
         main_layout.addWidget(add_note_button, 0, 0, 1, 1)  # Button in the first row, first column
@@ -112,18 +137,19 @@ class GUI(QtWidgets.QMainWindow):
         col = 0
 
         for index, (key, value) in enumerate(self.notepad.notes.items()):
-            label_input = QtWidgets.QLineEdit(key)
+            label = QtWidgets.QLabel(f"{key}:")
+            label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)  # Set alignment to right
             text_edit = QtWidgets.QTextEdit()
             text_edit.setPlainText(value)
-            text_edit.setReadOnly(True)
+            text_edit.setReadOnly(False)
 
-            button_edit = QtWidgets.QPushButton("Edit")
-            button_delete = QtWidgets.QPushButton("Delete")
+            button_edit = QtWidgets.QPushButton("Guardar")
+            button_delete = QtWidgets.QPushButton("Borrar")
 
-            button_edit.clicked.connect(lambda k=key, v=value: self.notepad.edit_note(k))
-            button_delete.clicked.connect(lambda k=key, v=value: self.notepad.delete_note(k))
+            button_edit.clicked.connect(lambda name=key, obj=text_edit: self.save_note(name, obj))
+            button_delete.clicked.connect(lambda name=key, v=value: self.delete_note(name))
 
-            scroll_layout.addWidget(label_input, row, col, 1, 1)
+            scroll_layout.addWidget(label, row, col, 1, 1)
             scroll_layout.addWidget(button_edit, row, col + 1, 1, 1)
             scroll_layout.addWidget(button_delete, row, col + 2, 1, 1)
             scroll_layout.addWidget(text_edit, row + 1, col, 1, 3)  # Span 3 columns for QTextEdit
@@ -143,14 +169,46 @@ class GUI(QtWidgets.QMainWindow):
         # Spanning three columns to avoid bugs
         main_layout.addWidget(scroll_area, 1, 0, 1, 3)
 
-    def add_note(self):
-        # capture name
-        # create file
-        # save file
-        # reload layout
-        pass
+    def reload_layout(self):
+        """Reload the layout by destroying it and calling to recreate it"""
+        # Clear existing layout
+        central_widget = self.centralWidget()
+        if central_widget is not None:
+            layout = central_widget.layout()
+            if layout is not None:
+                while layout.count():
+                    item = layout.takeAt(0)
+                    if item.widget():
+                        item.widget().deleteLater()
+        # Rebuild the layout
+        self.create_layout()
 
-    def show_popup(self, message):
+    def delete_note(self, name: str) -> None:
+        """Delete the note and reload the layout"""
+        self.notepad.delete_note(name)
+        self.notepad.reload_notes()
+        self.reload_layout()
+
+    def save_note(self, name: str, obj: QtWidgets.QTextEdit) -> None:
+        """Save the text on the note and reload the layout"""
+        self.notepad.save_note(filename=name, value=obj.toPlainText())
+        self.notepad.reload_notes()
+        self.reload_layout()
+
+    def add_note(self) -> None:
+        """Implements the logic to add a new note"""
+        # Capture name of the new file by asking in a popup
+        name, ok = QInputDialog.getText(self, "Add Note", "Enter the name for the new note:", QLineEdit.Normal, "")
+        if ok and name.strip():
+            # Create that file
+            self.notepad.add_note(name)
+            # Reload notes and layout to also show the new file
+            self.notepad.reload_notes()
+            self.reload_layout()
+        else:
+            self.show_popup("Creación de Nota cancelada.")
+
+    def show_popup(self, message: str) -> None:
         """Show a Pop-up with a message"""
         QtWidgets.QMessageBox.information(self, "Information", message)
 
